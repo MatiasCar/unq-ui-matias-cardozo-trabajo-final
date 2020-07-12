22d4bb309490c1670fa47c6f0e3369200e10db45
matiascar <matiascardozo2795@gmail.com>
2020-07-12 17:11:31 -0300

    agregados iconos de juego

---
+++ src/contents/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Menu from '../contents/Menu'
+import '../styles/MainPage.css'
+
+
+function MainPage(){
+
+
+    return(
+
+        <div className="body-mainpage">
+            <p>main page</p>
+            <Menu/>
+        </div>
+
+    )
+}
+
+export default MainPage;
