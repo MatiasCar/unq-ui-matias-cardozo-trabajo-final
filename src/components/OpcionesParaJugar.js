053b789d537c3d11840c30626f4d8b5539597633
matiascar <matiascardozo2795@gmail.com>
2020-07-17 11:54:05 -0300

    terminada funcionalidad de juego, se puede jugar varias partidas contra el cpu

--- src/components/OpcionesParaJugar.js
+++ src/components/OpcionesParaJugar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../styles/OpcionesParaJugar.css'
-import {useState} from 'react'
 
 import Piedra from '../icons/piedra.svg'
 import Papel from '../icons/papel.svg'
@@ -21,7 +20,7 @@ function OpcionesParaJugar({consultaEleccion}){
         <div className="iconos-menu">
             <img className="iconos" src={Piedra} alt="Piedra" onClick={() => pasarEleccion('Piedra')}/>
             <img className="iconos" src={Papel} alt="Papel" onClick={() => pasarEleccion('Papel')}/>
-            <img className="iconos" src={Tijeras} alt="Tijeras" onClick={() => pasarEleccion('Tijeras')}/>
+            <img className="iconos" src={Tijeras} alt="Tijera" onClick={() => pasarEleccion('Tijera')}/>
             <img className="iconos" src={Lagarto} alt="Lagarto" onClick={() => pasarEleccion('Lagarto')}/>
             <img className="iconos" src={Spock} alt="Spock" onClick={() => pasarEleccion('Spock')}/>
             
--- src/components/SinglePlayer.js
+++ src/components/SinglePlayer.js
@@ -11,43 +11,113 @@ function SinglePlayer(){
     const [juegoTerminado, setJuegoTerminado]= useState(false);
 
     //CPU
-    const eleccionesPosibles = ['Piedra', 'Papel', 'Tijeras', 'Lagarto', 'Spock'];
-    const [eleccionCPU, setEleccionCPU] = useState('')
+    const eleccionesPosibles = ['Piedra', 'Papel', 'Tijera', 'Lagarto', 'Spock'];
+    const [eleccionCpu,setEleccionCPU] = useState('');
     const [ganadasCPU, setGanadasCPU]= useState(0);
+
+    const [ganadorDeRonda, setGanadorDeRonda]= useState('');
     
 
     function verificarGanador(opcJug1 , opcCPU){
 
         var rta= '';
-        if(opcJug1 ==='Piedra' && opcCPU=== 'Tijeras'){
-            rta= 'jug1';
+
+        //Casos para eleccion 'Piedra' del jugador
+        if(opcJug1 === 'Piedra'){
+            //gana contra:
+            if(opcCPU === 'Tijera' || opcCPU === 'Lagarto'){
+                rta = 'jug1'
+            }
+            //pierde contra:
+            if(opcCPU === 'Papel' || opcCPU === 'Spock'){
+                rta = 'jug2'
+            }
+            if(opcCPU === 'Piedra'){
+                rta = 'empate'
+            }
             return rta;
         }
-         if(opcJug1 ==='Piedra' && opcCPU=== 'Lagarto'){
-           rta='jug1';
-           return rta;
-        }
-        if(opcJug1 ==='Piedra' && opcCPU=== 'Piedra'){
-           rta='empate';
-           return rta;
-        }
-        else{
-            rta='jug2';
+
+        //Casos para eleccion 'Papel' del jugador
+        if(opcJug1 === 'Papel'){
+            //gana contra:
+            if(opcCPU === 'Piedra' || opcCPU === 'Spock'){
+                rta = 'jug1'
+            }
+            //pierde contra:
+            if(opcCPU === 'Tijera' || opcCPU === 'Lagarto'){
+                rta = 'jug2'
+            }
+            if(opcCPU === 'Papel'){
+                rta = 'empate'
+            }
             return rta;
         }
-        
+        //Casos para eleccion 'Tijera' del jugador
+        if(opcJug1 === 'Tijera'){
+            //gana contra:
+            if(opcCPU === 'Papel' || opcCPU === 'Lagarto'){
+                rta = 'jug1'
+            }
+            //pierde contra:
+            if(opcCPU === 'Piedra' || opcCPU === 'Spock'){
+                rta = 'jug2'
+            }
+            if(opcCPU === 'Tijera'){
+                rta = 'empate'
+            }
+            return rta;
+        }
+        //Casos para eleccion 'Lagarto' del jugador
+        if(opcJug1 === 'Lagarto'){
+            //gana contra:
+            if(opcCPU === 'Papel' || opcCPU === 'Spock'){
+                rta = 'jug1'
+            }
+            //pierde contra:
+            if(opcCPU === 'Piedra' || opcCPU === 'Tijera'){
+                rta = 'jug2'
+            }
+            if(opcCPU === 'Lagarto'){
+                rta = 'empate'
+            }
+            return rta;
+        }
+        //Casos para eleccion 'Spock' del jugador
+        if(opcJug1 === 'Spock'){
+            //gana contra:
+            if(opcCPU === 'Tijera' || opcCPU === 'Piedra'){
+                rta = 'jug1'
+            }
+            //pierde contra:
+            if(opcCPU === 'Lagarto' || opcCPU === 'Papel'){
+                rta = 'jug2'
+            }
+            if(opcCPU === 'Spock'){
+                rta = 'empate'
+            }
+            return rta;
+        }
+ 
     }
 
-    function jugar(opcJug1 , opcCPU){
-        if(!juegoTerminado){
-            var eleccionRandom= eleccionesPosibles[Math.floor(Math.random() * eleccionesPosibles.length)];
+    function jugar(opcJug1){
+        if(!juegoTerminado && opcJug1 !==''){
+            //Seteo una eleccion random para el cpu
+            var eleccionRandom = eleccionesPosibles[Math.floor(Math.random() * eleccionesPosibles.length)]
             setEleccionCPU(eleccionRandom);
-            var ganador =verificarGanador(opcJug1,opcCPU);
+            
+            var ganador =verificarGanador(opcJug1, eleccionRandom);
             if (ganador ==='jug1'){
                 setGanadasJugador(ganadasJugador+1)
+                setGanadorDeRonda('Ganador Jugador 1')
             }
             if(ganador==='jug2'){
                 setGanadasCPU(ganadasCPU+1)
+                setGanadorDeRonda('Ganador CPU')
+            }
+            if(ganador === 'empate'){
+                setGanadorDeRonda('Empate')
             }
             console.log(ganador)
             setJuegoTerminado(true)
@@ -62,6 +132,7 @@ function SinglePlayer(){
         setEleccionjugador('');
         setEleccionCPU('');
         setJuegoTerminado(false);
+        setGanadorDeRonda('');
     }
 
 
@@ -72,13 +143,14 @@ function SinglePlayer(){
     return(
         <div>
             <Player verEleccion={verEleccion}/>
-            {/* <Player/> */}
-            <button className="boton-jugar" type="button" onClick={() => jugar(eleccionJugador,eleccionCPU)}>Jugar</button>
+            <button className="boton-jugar" type="button" onClick={() => jugar(eleccionJugador)}>Jugar</button>
             <button className="boton-jugar" type="button" onClick={()=>reset()} >Jugar de Nuevo</button>
-            <h1>El CPU eligió : {eleccionCPU}</h1>
+            <h1>Eleccion del Jugador: {eleccionJugador}</h1>
+            <h1>El CPU eligió : {eleccionCpu}</h1>
+            <h2>Resultado final: {ganadorDeRonda}</h2>
             <h3>Ganadas CPU: {ganadasCPU}</h3>
-            <h3>Ganadas Jug: {ganadasJugador}</h3>
-            <h3>eleccion single: {eleccionJugador}</h3>
+            <h3>Ganadas Jugador: {ganadasJugador}</h3>
+            
         </div>
     )
 }
